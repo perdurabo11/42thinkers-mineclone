1ef525ab8548e0c628497a05b7b2e5e1b6b45903
perdurabo11 <georg.goldes@live.de>
2016-06-06 09:28:50 +0200

    Rejected commit: Default

--- Algorithmen.java
+++ Algorithmen.java
@@ -1,33 +1,45 @@
 import java.util.Random;
 
+
 /**
  * Created by perdu on 20.05.16.
  */
 public class Algorithmen
 {
+int derzeitigesLevel =0;  // immo nur ein helper
 
-    private void PlaziereBomben (int breite,int hoehe)
+
+    private void PlaziereBomben (int x ,int y )
     {
         Random rand = new Random();
+        int b;
+        int c;
+        int d;
+        b = Level.getBreite()[derzeitigesLevel];
+        c = Level.getHoehe()[derzeitigesLevel];
+        d = Level.getBomben()[derzeitigesLevel];
 
-        minenLand = new int[Level.breite][Level.hoehe]; // minenland wird durch die Dimensionen  des Lvls bestimmt
+         int [][]minenLand = new int[b][c]; // minenland wird durch die Dimensionen  des Lvls bestimmt
 
         int zähler = 0;
         int xPunkt;
         int yPunkt;
-        while(zähler<Level.Bomben){ //performanter als die andere Lösung
-            xPunkt = rand.nextInt(Level.breite);
-            yPunkt = rand.nextInt(Level.hoehe);
-            if ((minenLand[xPunkt][yPunkt] != 10) &!=22) // da passt noch was nicht! Abfrage ob keine 10 oder keine 22
-            {
-                minenLand[xPunkt][yPunkt]= 10; // 10 ist eine Bombe
-                zähler++;
-
+        while(zähler<d){ //performanter als die andere Lösung
+            xPunkt = rand.nextInt(b);
+            yPunkt = rand.nextInt(c);
+            if ((minenLand[xPunkt][yPunkt] != 10)) {
+                    if (minenLand[xPunkt][yPunkt]!=22)
+                    {
+                        minenLand[xPunkt][yPunkt] = 10; // 10 ist eine Bombe
+                        zähler++;
+                    }
             }
         }
+
+
        //Minenzähler im Umfeld
-        for (int i = 0; i < Level.breite; i++) {
-            for (int j = 0; j < Level.hoehe; j++) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < c; j++) {
                 if (minenLand[i][j]==10 ) {            //  Abfrage ob Mine da ist
                     for (int k = -1; k <= 1 ; k++) {
                         for (int l = -1; l <= 1; l++) {
